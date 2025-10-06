43f2e3d735e70af8569d42ce81f48d9f127f446e
RevanzaRabbani <revanzasapta21@gmail.com>
2025-10-06 19:42:29 +0700

    update

--- Modul04/Jurnal01.java
+++ Modul04/Jurnal01.java
@@ -4,18 +4,5 @@ public class Jurnal01 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
-        Scanner input = new Scanner(System.in);
-
-        //Input bilangan bulat N
-        int nilaiN = input.nextInt();
-
-        // cek apakah nilaiN kelipatan enam 
-        if (nilaiN % 6 == 0) {
-            System.out.println("kelipatan enam");
-        } else {
-            System.out.println("Bukan Kelipatan enam");
-        }
-
-        input.close();
     }
 }
--- Modul04/TP02.java
+++ Modul04/TP02.java
@@ -1,6 +1,46 @@
-public class TP02 {
+import java.util.Scanner;
+
+public class Jurnal02 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+
+        //input bilangan bulat N
+        int bilanganBulatN = input.nextInt();
+
+        //variabel double untuk perhitungan desimal 
+        double diskon = 0;
+        double totalsetelahDiskon;
+        double pajak;
+        double totalDibayar;
+
+        // jika transaksi lebih dari 200000, maka diskon 5%
+        if (bilanganBulatN > 200000){
+            diskon = bilanganBulatN * 0.05;
+        }
+
+        //total setelah diskon 
+        totalsetelahDiskon = bilanganBulatN - diskon;
+
+        //hitunglah pajak setalah diskon
+        pajak = totalsetelahDiskon *0.11;
+
+        //total akhir 
+        totalDibayar = totalsetelahDiskon + pajak;
+
+        //hasil tampilan 
+        System.out.println("transaksi = " + bilanganBulatN);
+        if (diskon > 0){
+            //baris ini hanya berjalan jika ada diskon 
+            System.out.println("total = " + bilanganBulatN + " - " + (int)diskon);
+        } else {
+            //baris ini hanya berjalan jika tidak ada diskon
+            System.out.println("total = " + bilanganBulatN);    
+        }
+        System.out.println("Ppn 11% = " + pajak);
+        System.out.println("total dibayar = " + totalDibayar);
+
+        input.close();
     }
 }

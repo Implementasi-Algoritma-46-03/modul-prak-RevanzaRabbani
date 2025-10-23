befcfda3635c503f159e516d0030da41b5e25aba
RevanzaRabbani <revanzasapta21@gmail.com>
2025-10-23 16:26:55 +0700

    perbaikan

--- Modul06/TP03.java
+++ Modul06/TP03.java
@@ -1,78 +1,89 @@
-import java.util.Scanner; 
+import java.util.Scanner;
 
 public class TP03 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); 
 
-        // User memasukkan nama bangun datar
-        String nama = input.nextLine();
+    // Method untuk menghitung luas dan keliling persegi
+    public static int luasPersegi(int sisi) {
+        return sisi * sisi;
+    }
 
-        switch (nama.toLowerCase()) { 
-            case "persegi":
+    public static int kelilingPersegi(int sisi) {
+        return 4 * sisi;
+    }
+
+    // Method untuk menghitung luas dan keliling persegi panjang
+    public static int luasPersegiPanjang(int panjang, int lebar) {
+        return panjang * lebar;
+    }
+
+    public static int kelilingPersegiPanjang(int panjang, int lebar) {
+        return 2 * (panjang + lebar);
+    }
+
+    // Method untuk menghitung luas dan keliling segitiga (siku-siku)
+    public static double luasSegitiga(int alas, int tinggi) {
+        return 0.5 * alas * tinggi;
+    }
+
+    public static double kelilingSegitiga(int alas, int tinggi) {
+        double miring = Math.sqrt(alas * alas + tinggi * tinggi);
+        return alas + tinggi + miring;
+    }
+
+    // Method untuk menghitung luas dan keliling lingkaran
+    public static double luasLingkaran(int diameter) {
+        double jariJari = diameter / 2.0;
+        return 3.14 * jariJari * jariJari;
+    }
+
+    public static double kelilingLingkaran(int diameter) {
+        return 3.14 * diameter;
+    }
+
+    public static void main(final String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String namaBangunDatar = input.nextLine();
+
+        double luas = 0;
+        double keliling = 0;
+
+        switch (namaBangunDatar) {
+            case "Persegi":
                 int sisi = input.nextInt();
-                tampilkanHasil(hitungLuasPersegi(sisi), hitungKelilingPersegi(sisi));
+                luas = luasPersegi(sisi);
+                keliling = kelilingPersegi(sisi);
+                System.out.printf("%d %d", (int) luas, (int) keliling);
                 break;
-
-            case "persegi panjang":
+            
+            case "Persegi Panjang":
                 int panjang = input.nextInt();
                 int lebar = input.nextInt();
-                tampilkanHasil(hitungLuasPersegiPanjang(panjang, lebar), hitungKelilingPersegiPanjang(panjang, lebar));
+                luas = luasPersegiPanjang(panjang, lebar);
+                keliling = kelilingPersegiPanjang(panjang, lebar);
+                System.out.printf("%d %d", (int) luas, (int) keliling);
                 break;
 
-            case "segitiga":
+            case "Segitiga":
                 int alas = input.nextInt();
                 int tinggi = input.nextInt();
-                tampilkanHasil(hitungLuasSegitiga(alas, tinggi), hitungKelilingSegitiga(alas, tinggi));
+                luas = luasSegitiga(alas, tinggi);
+                keliling = kelilingSegitiga(alas, tinggi);
+                System.out.printf("%d %d", (int) luas, (int) keliling);
                 break;
 
-            case "lingkaran":
+            case "Lingkaran":
                 int diameter = input.nextInt();
-                tampilkanHasil(hitungLuasLingkaran(diameter), hitungKelilingLingkaran(diameter));
+                luas = luasLingkaran(diameter);
+                keliling = kelilingLingkaran(diameter);
+                System.out.printf("%.2f %.2f", luas, keliling);
                 break;
 
             default:
                 System.out.println("Bangun datar tidak dikenal");
+                return;
         }
-
+        
         input.close();
-    }
-
-    private static void tampilkanHasil(double luas, double keliling) {
-        // Hasil tanpa desimal (dibulatkan)
-        System.out.printf("%.0f %.0f\n", luas, keliling);
-    }
-
-    private static double hitungLuasPersegi(int sisi) {
-        return sisi * sisi;
-    }
-
-    private static double hitungKelilingPersegi(int sisi) {
-        return 4 * sisi;
-    }
-
-    private static double hitungLuasPersegiPanjang(int p, int l) {
-        return p * l;
-    }
-
-    private static double hitungKelilingPersegiPanjang(int p, int l) {
-        return 2 * (p + l);
-    }
-
-    private static double hitungLuasSegitiga(int alas, int tinggi) {
-        return 0.5 * alas * tinggi;
-    }
-
-    private static double hitungKelilingSegitiga(int alas, int tinggi) {
-        double miring = Math.sqrt(Math.pow(alas, 2) + Math.pow(tinggi, 2));
-        return alas + tinggi + miring;
-    }
-
-    private static double hitungLuasLingkaran(int diameter) {
-        double r = diameter / 2.0;
-        return 3.14 * r * r;
-    }
-
-    private static double hitungKelilingLingkaran(int diameter) {
-        return 3.14 * diameter;
     }
 }

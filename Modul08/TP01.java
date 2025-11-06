451bbd416479b625a96333258a1ae76e1c01f966
RevanzaRabbani <revanzasapta21@gmail.com>
2025-11-06 21:45:55 +0700

    perbaikan

--- Modul08/TP01.java
+++ Modul08/TP01.java
@@ -2,15 +2,14 @@ import java.util.Scanner;
 
 public class TP01 {
     public static void main(String[] args) {
-        
         Scanner s = new Scanner(System.in);
         int target = s.nextInt();
         int amoeba = 30;
         int waktu = 0;
 
         while (amoeba < target) {
-            amoeba = 2;
-            waktu += 15;
+            amoeba *= 2;   // amoeba membelah diri jadi 2x lipat
+            waktu += 15;   // setiap pembelahan butuh 15 menit
         }
 
         System.out.println(waktu);

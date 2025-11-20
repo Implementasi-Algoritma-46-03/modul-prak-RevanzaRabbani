aaf06080f16b755d616e9ba6dee33c63a9f471e2
RevanzaRabbani <revanzasapta21@gmail.com>
2025-11-20 16:07:39 +0700

    perbaikan

--- Modul10/TP02.java
+++ Modul10/TP02.java
@@ -3,26 +3,27 @@ import java.util.Scanner;
 public class TP02 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+
         Scanner in = new Scanner(System.in);
 
         int masukan = in.nextInt();
 
+        // Bagian naik
         for (int i = 1; i <= masukan ; i++){
             for (int j = 1; j < i; j++){
                 System.out.print(" ");
-                }
-                System.out.println(i);  
-        }
-        
-        for (int i = masukan - 1; i < masukan; i--){
-            for (int s = 1; s < i; s++){
-                    System.out.print(" ");
             }
-            if (i == 0){
-                break;
+            System.out.println(i);  
+        }
+
+        // Bagian turun
+        for (int i = masukan - 1; i >= 1; i--){
+            for (int s = 1; s < i; s++){
+                System.out.print(" ");
             }
             System.out.println(i);
         }
+
+        in.close();
     }
 }

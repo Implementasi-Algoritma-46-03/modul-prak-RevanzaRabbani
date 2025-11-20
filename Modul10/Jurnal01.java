1927f07d3b7c2808513d3551f6ca86fe9aff270e
RevanzaRabbani <revanzasapta21@gmail.com>
2025-11-20 17:14:33 +0700

    perbaikan

--- Modul10/Jurnal01.java
+++ Modul10/Jurnal01.java
@@ -3,14 +3,15 @@ public class Jurnal01 {
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
         // For loop untuk membuat baris ke bawah
-        for (int i = 1; i <= 5; i++) {
-            // Nested If untuk membuat spasi di 2, 4, dan 6
-            if (i % 2 == 0) {
-                System.out.print(" ");
-            }
+        for (int i = 1; i <= 6; i++) {
             // Nested For untuk membuat kolom baris ke kanan
-            for (int j = 1; j <= 6; j++) {
+            for (int j = 1; j <= 5; j++) {
+                // Nested If untuk membuat spasi di 2, 4, dan 6
+            if (i % 2 == 0) {
+                System.out.print(" " + i);
+            }else{
                 System.out.print(i + " ");
+            }
             }
 
             // Menampilkan hasil

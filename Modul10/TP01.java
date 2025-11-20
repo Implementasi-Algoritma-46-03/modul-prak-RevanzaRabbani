f7d20592e1f0fb7f3132e77eb081e438902627f4
RevanzaRabbani <revanzasapta21@gmail.com>
2025-11-20 15:50:29 +0700

    perbaikan

--- Modul10/TP01.java
+++ Modul10/TP01.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
 public class TP01 {
 
     public static void main(final String[] args) {
         // Kerjakan soalnya di sini
+        for(int i=5;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }

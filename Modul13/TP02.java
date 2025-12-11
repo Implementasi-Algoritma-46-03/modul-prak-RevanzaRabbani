6caca19d90f3200ab26d001d3436577d31b5b921
RevanzaRabbani <revanzasapta21@gmail.com>
2025-12-11 15:59:02 +0700

    perubahan

--- Modul13/TP02.java
+++ Modul13/TP02.java
@@ -1,6 +1,44 @@
-public class TP02 {
+import java.util.*;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class TP02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double[] wave1 = new double[10];
+        double[] wave2 = new double[10];
+
+        for (int i = 0; i < 10; i++) {
+            wave1[i] = input.nextDouble();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            wave2[i] = input.nextDouble();
+        }
+
+        bubbleSortDescending(wave1);
+        bubbleSortDescending(wave2);
+
+        printArray(wave1);
+        printArray(wave2);
+    }
+
+    public static void bubbleSortDescending(double[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    double temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(double[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf("%.2f", arr[i]);
+            if (i < arr.length - 1) System.out.print(" ");
+        }
+        System.out.println();
     }
 }
--- Modul13/TP03.java
+++ Modul13/TP03.java
@@ -1,6 +1,30 @@
-public class TP03 {
+import java.util.*;
 
-    public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+public class TP03 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int[] data = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            data[i] = input.nextInt();
+        }
+
+        int key = input.nextInt();
+
+        int indeks = -1;
+        for (int i = 0; i < n; i++) {
+            if (data[i] == key) {
+                indeks = i;
+                break;
+            }
+        }
+
+        if (indeks != -1) {
+            System.out.println("Ditemukan di indeks ke-" + indeks);
+        } else {
+            System.out.println("Tidak ditemukan");
+        }
     }
 }
